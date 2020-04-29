49f94baada0ca814807a5b25128f35fd607221ed
Haguro-zz <hugo.ols@outlook.com>
2020-04-29 16:51:19 -0300

    Contador de Strings

--- hello_world.py
+++ hello_world.py
@@ -12,16 +12,22 @@ print("""
 print('-' * 85)
 
 #Formatação de string dentro de uma mensagem utilizando Chaves
-nome = input('Olá, digite o que vier à mente: ')
-#print('Seja bem vindo, {}'.format(nome))
+nome = input('Olá, digite o seu nome: ')
+capitalizado = nome.title()
+
+primeiroNome = capitalizado.split() #Divide o texto em uma lista, separando cada palavra, identificada pelo espaço.
+nomeCompleto = ' '.join(primeiroNome) #Junta as palavras novamente, adicionando espaços entre as palavras separadas pelo split().
+
+print('Seja bem vindo, {}'.format(primeiroNome[0]))
+print('Seu nome completo é', nomeCompleto)
 
 #Informações possíveis sobre a digitação
-print('Algumas informações técnicas sobre a frase:')
-print('O tipo primitivo digitado é: ', type(nome))
-print('Contém somente espaços? ', nome.isspace())
-print('É um número? ', nome.isnumeric())
-print('É alfabético? ', nome.isalpha())
-print('É alfanúmerico? ', nome.isalnum())
-print('Todos os caracteres são maiúsculos? ', nome.isupper())
-print('Todos os caracteres são minúsculos?', nome.islower())
-print('O texto está capitalizado (inicial maiúscula)? ', nome.istitle())
+#print('Algumas informações técnicas sobre a frase:')
+#print('O tipo primitivo digitado é: ', type(nome))
+#print('Contém somente espaços? ', nome.isspace())
+#print('É um número? ', nome.isnumeric())
+#print('É alfabético? ', nome.isalpha())
+#print('É alfanúmerico? ', nome.isalnum())
+#print('Todos os caracteres são maiúsculos? ', nome.isupper())
+#print('Todos os caracteres são minúsculos?', nome.islower())
+#print('O texto está capitalizado (inicial maiúscula)? ', nome.istitle())
