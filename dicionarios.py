5712aa44bee3723d2dccf5e22b1a428fe73be952
Haguro-zz <hugo.ols@outlook.com>
2020-05-15 15:27:57 -0300

    Dicionários

--- dicionarios.py
+++ dicionarios.py
@@ -9,17 +9,28 @@
 from random import randrange
 from time import sleep
 
-jogadores = {}
-listaJogador = []
+jogadores = {} # Inicia o dicionário
+listaJogador = [] # Inicia a lista que conterá o dicionário
 
 quantJogador = int(input('Quantos jogadores participarão? '))
 
 for j in range(0,quantJogador):
-    jogadores['nome'] = (str(input('Digite o nome do jogador: ')))
+    jogadores['nome'] = (str(input('Digite o nome do jogador: ')).capitalize())
     jogadores['dado'] = randrange(1,7,1, int)
-    listaJogador.append(jogadores.copy())
+    listaJogador.append(jogadores.copy()) # Adiciona os valores recebidos no dicionário à lista
 
 for c in listaJogador:
     for v in c.values():
         print(f'{c["nome"]} tirou {c["dado"]}!')
+        sleep(0.5)
+        break
+
+listaJogador.sort(key=lambda item: item.get("dado"), reverse=True) # Organiza a lista pelo valor do dado
+
+print(f'\n{" RANKING ":=^20}\n')
+
+for c in listaJogador:
+    for v in c.values():
+        print(f'{c["nome"]} com {c["dado"]} no dado!')
+        sleep(0.5)
         break
