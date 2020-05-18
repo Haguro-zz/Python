ee73e2360034778d3492bcbfd6fe0281afa70b44
Haguro-zz <hugo.ols@outlook.com>
2020-05-17 22:10:49 -0300

    Funções

--- dicionarios.py
+++ dicionarios.py
@@ -9,28 +9,46 @@
 from random import randrange
 from time import sleep
 
-jogadores = {} # Inicia o dicionário
-listaJogador = [] # Inicia a lista que conterá o dicionário
+def dados(quantJogador):
 
-quantJogador = int(input('Quantos jogadores participarão? '))
+    jogadores = {} # Inicia o dicionário
+    listaJogador = [] # Inicia a lista que conterá o dicionário
 
-for j in range(0,quantJogador):
-    jogadores['nome'] = (str(input('Digite o nome do jogador: ')).capitalize())
-    jogadores['dado'] = randrange(1,7,1, int)
-    listaJogador.append(jogadores.copy()) # Adiciona os valores recebidos no dicionário à lista
+#    quantJogador = int(input('Quantos jogadores participarão? '))
 
-for c in listaJogador:
-    for v in c.values():
-        print(f'{c["nome"]} tirou {c["dado"]}!')
-        sleep(0.5)
-        break
+    for j in range(0,quantJogador):
+        jogadores['nome'] = (str(input('Digite o nome do jogador: ')).capitalize())
+        jogadores['dado'] = randrange(1,7,1, int)
+        listaJogador.append(jogadores.copy()) # Adiciona os valores recebidos no dicionário à lista
+    print('')
+    for c in listaJogador:
+        for v in c.values():
+            print(f'{c["nome"]:^10} tirou {c["dado"]}!')
+            sleep(0.5)
+            break
 
-listaJogador.sort(key=lambda item: item.get("dado"), reverse=True) # Organiza a lista pelo valor do dado
+    listaJogador.sort(key=lambda item: item.get("dado"), reverse=True) # Organiza a lista pelo valor do dado
 
-print(f'\n{" RANKING ":=^20}\n')
+    print(f'\n{" RANKING ":=^30}\n')
 
-for c in listaJogador:
-    for v in c.values():
-        print(f'{c["nome"]} com {c["dado"]} no dado!')
-        sleep(0.5)
+    for c in listaJogador:
+        for v in c.values():
+            print(f'{c["nome"]:^10} com {c["dado"]} no dado!')
+            sleep(0.5)
+            break
+
+#Programa Principal
+
+numeroJogadores = int(input('Quantos jogadores irão participar? '))
+
+dados(numeroJogadores)
+
+while True:
+    jogarNovamente = str(input('\nDeseja sortear novos números? \033[2;31m[S/N]\033[m ')).strip().upper()[0]
+
+    if jogarNovamente in 'S':
+        numeroJogadores = int(input('Quantos jogadores irão participar? '))
+        dados(numeroJogadores)
+    else:
+        print('\n\033[1mAté mais!')
         break
--- menu.py
+++ menu.py
@@ -6,67 +6,70 @@ import random
 
 # Estrutura de menu
 
-corLetra = random.randrange(30,37)
-texto = 'Bem-vindo(a)'
+def menu():
+    corLetra = random.randrange(30,37)
+    texto = 'Bem-vindo(a)'
 
-print(f'''\033[1;{corLetra}m
-███    ███ ███████ ███    ██ ██    ██ 
-████  ████ ██      ████   ██ ██    ██ 
-██ ████ ██ █████   ██ ██  ██ ██    ██ 
-██  ██  ██ ██      ██  ██ ██ ██    ██ 
-██      ██ ███████ ██   ████  ██████ \033[m''')
+    print(f'''\033[1;{corLetra}m
+    ███    ███ ███████ ███    ██ ██    ██ 
+    ████  ████ ██      ████   ██ ██    ██ 
+    ██ ████ ██ █████   ██ ██  ██ ██    ██ 
+    ██  ██  ██ ██      ██  ██ ██ ██    ██ 
+    ██      ██ ███████ ██   ████  ██████ \033[m''')
 
-print(f'\n{texto:=^35}')
+    print(f'\n{texto:=^35}')
 
-menu = ('''
-1) Contagem Regressiva
-2) Poema
-3) Data
-0) Sair
-''')
+    menu = ('''
+    1) Contagem Regressiva
+    2) Poema
+    3) Data
+    0) Sair
+    ''')
 
-print(menu)
+    print(menu)
 
-data = datetime.datetime.now()
+    data = datetime.datetime.now()
 
-while True:
+    while True:
+
+        opcao = int(input('Digite a opção desejada (`10` para ver o menu): '))
+
+        if opcao == 10:
+            print(menu)
+
+        elif opcao == 1:
+            for c in range(10, 0, -1):
+                print(c)
+                sleep(0.1)  # Timer para execução do laço
+            print('\033[1;31m\nACABOU!!!\033[m')
+
+        elif opcao == 2:
+            print('''\033[1m
+            If the water forms
+            the forms of the weeds, there—
     
-    opcao = int(input('Digite a opção desejada (`10` para ver o menu): '))
+            a long life is not by that
+            a necessarily happy one.
     
-    if opcao == 10:
-        print(menu)
+            My friend. We
+            reckon on a simple
     
-    elif opcao == 1:
-        for c in range(10, 0, -1):
-            print(c)
-            sleep(0.1)  # Timer para execução do laço
-        print('\033[1;31m\nACABOU!!!\033[m')
+            agreement,
+            the fashion of a stone
+    
+            underground.\033[m''')
 
-    elif opcao == 2:
-        print('''\033[1m
-        If the water forms
-        the forms of the weeds, there—
+        elif opcao == 3:
+            print(data)
 
-        a long life is not by that
-        a necessarily happy one.
+        elif opcao == 0:
+            break
 
-        My friend. We
-        reckon on a simple
+        else:
+            print('\033[1;31m\nOpção inválida!\033[m')
+            print('*' * 20)
+            opcao = int(input('Digite a opção desejada: '))
 
-        agreement,
-        the fashion of a stone
+    print('\nAté logo!')
 
-        underground.\033[m''')
-
-    elif opcao == 3:
-        print(data)
-
-    elif opcao == 0:
-        break
-
-    else:
-        print('\033[1;31m\nOpção inválida!\033[m')
-        print('*' * 20)
-        opcao = int(input('Digite a opção desejada: '))
-
-print('\nAté logo!')
+menu()
